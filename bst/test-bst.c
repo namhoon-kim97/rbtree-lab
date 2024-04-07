b29e308b584bff960cd470f4f96d2025dc234649
Ubuntu <ubuntu@ip-172-31-2-119.ap-northeast-2.compute.internal>
2024-04-07 14:33:44 +0000

    rbtree init, delete, insert 구현

--- bst/test-bst.c
+++ bst/test-bst.c
@@ -1,105 +1,106 @@
 #include "bst.h"
-#include <stdio.h>
 #include <assert.h>
+#include <stdio.h>
 #include <stdlib.h>
 
 void test_init(void) {
-    bst *t = new_bst();
-    assert(t != NULL);
+  bst *t = new_bst();
+  assert(t != NULL);
 
-    assert(t->root == NULL);
-    delete_bst(t);
+  assert(t->root == NULL);
+  delete_bst(t);
 }
 
 void test_insert_single(const key_t key) {
-    bst *t = new_bst();
-    node_t *p = bst_insert(t, key);
-    assert(p != NULL);
-    assert(t->root == p);
-    assert(p->key == key);
+  bst *t = new_bst();
+  node_t *p = bst_insert(t, key);
+  assert(p != NULL);
+  assert(t->root == p);
+  assert(p->key == key);
 
-    assert(p->left == NULL);
-    assert(p->right == NULL);
+  assert(p->left == NULL);
+  assert(p->right == NULL);
 
-    delete_bst(t);
+  delete_bst(t);
 }
 
 void test_find_single(const key_t key, const key_t wrong_key) {
-    bst *t = new_bst();
-    node_t *p = bst_insert(t, key);
+  bst *t = new_bst();
+  node_t *p = bst_insert(t, key);
 
-    node_t *q = bst_find(t, key);
-    assert(q != NULL);
-    assert(q->key == key);
-    assert(q == p);
+  node_t *q = bst_find(t, key);
+  assert(q != NULL);
+  assert(q->key == key);
+  assert(q == p);
 
-    q = bst_find(t, wrong_key);
-    assert(q == NULL);
+  q = bst_find(t, wrong_key);
+  assert(q == NULL);
 
-    delete_bst(t);
+  delete_bst(t);
 }
 
 static void insert_arr(bst *t, const key_t *arr, const size_t n) {
-    for (size_t i = 0; i < n; i++) {
-        bst_insert(t, arr[i]);
-    }
+  for (size_t i = 0; i < n; i++) {
+    bst_insert(t, arr[i]);
+  }
 }
 
 static int comp(const void *p1, const void *p2) {
-    const key_t *e1 = (const key_t *)p1;
-    const key_t *e2 = (const key_t *)p2;
-    if (*e1 < *e2) {
-        return -1;
-    } else if (*e1 > *e2) {
-        return 1;
-    } else {
-        return 0;
-    }
+  const key_t *e1 = (const key_t *)p1;
+  const key_t *e2 = (const key_t *)p2;
+  if (*e1 < *e2) {
+    return -1;
+  } else if (*e1 > *e2) {
+    return 1;
+  } else {
+    return 0;
+  }
 };
 
 // min/max should return the min/max value of the tree
 void test_minmax(key_t *arr, const size_t n) {
-    // null array is not allowed
-    assert(n > 0 && arr != NULL);
+  // null array is not allowed
+  assert(n > 0 && arr != NULL);
 
-    bst *t = new_bst();
-    assert(t != NULL);
+  bst *t = new_bst();
+  assert(t != NULL);
 
-    insert_arr(t, arr, n);
-    assert(t->root != NULL);
+  insert_arr(t, arr, n);
+  assert(t->root != NULL);
 
-    qsort((void *)arr, n, sizeof(key_t), comp);
-    node_t *p = bst_min(t);
-    assert(p != NULL);
-    assert(p->key == arr[0]);
+  qsort((void *)arr, n, sizeof(key_t), comp);
+  node_t *p = bst_min(t);
+  assert(p != NULL);
+  assert(p->key == arr[0]);
 
-    node_t *q = bst_max(t);
+  node_t *q = bst_max(t);
+  assert(q != NULL);
+  assert(q->key == arr[n - 1]);
+
+  bst_erase(t, p);
+  p = bst_min(t);
+  assert(p != NULL);
+  assert(p->key == arr[1]);
+
+  if (n >= 2) {
+    bst_erase(t, q);
+    q = bst_max(t);
     assert(q != NULL);
-    assert(q->key == arr[n - 1]);
+    assert(q->key == arr[n - 2]);
+  }
 
-    bst_erase(t, p);
-    p = bst_min(t);
-    assert(p != NULL);
-    assert(p->key == arr[1]);
-
-    if (n >= 2) {
-        bst_erase(t, q);
-        q = bst_max(t);
-        assert(q != NULL);
-        assert(q->key == arr[n - 2]);
-    }
-
-    delete_bst(t);
+  delete_bst(t);
 }
 
 void test_minmax_suite() {
-    key_t entries[] = {10, 5, 8, 34, 67, 23, 156, 24, 2, 12};
-    const size_t n = sizeof(entries) / sizeof(entries[0]);
-    test_minmax(entries, n);
+  key_t entries[] = {10, 5, 8, 34, 67, 23, 156, 24, 2, 12};
+  const size_t n = sizeof(entries) / sizeof(entries[0]);
+  test_minmax(entries, n);
 }
 
-static void inorder(node_t *n){
-  if (!n) return;
+static void inorder(node_t *n) {
+  if (!n)
+    return;
 
   inorder(n->left);
   printf("%d ", n->key);
@@ -107,73 +108,71 @@ static void inorder(node_t *n){
 }
 
 void test_find_erase(bst *t, const key_t *arr, const size_t n) {
-    for (int i = 0; i < n; i++) {
-        node_t *p = bst_insert(t, arr[i]);
-        assert(p != NULL);
-    }
-    
-    // 중위순회 트리 출력
-    inorder(t->root);
+  for (int i = 0; i < n; i++) {
+    node_t *p = bst_insert(t, arr[i]);
+    assert(p != NULL);
+  }
 
-    for (int i = 0; i < n; i++) {
-        node_t *p = bst_find(t, arr[i]);
-        //printf("arr[%d] = %d\n", i, arr[i]);
-        assert(p != NULL);
-        assert(p->key == arr[i]);
-        bst_erase(t, p);
-    }
+  // 중위순회 트리 출력
+  // inorder(t->root);
 
-    for (int i = 0; i < n; i++) {
-        node_t *p = bst_find(t, arr[i]);
-        assert(p == NULL);
-    }
+  for (int i = 0; i < n; i++) {
+    node_t *p = bst_find(t, arr[i]);
+    // printf("arr[%d] = %d\n", i, arr[i]);
+    assert(p != NULL);
+    assert(p->key == arr[i]);
+    bst_erase(t, p);
+  }
 
-    for (int i = 0; i < n; i++) {
-        node_t *p = bst_insert(t, arr[i]);
-        assert(p != NULL);
-        node_t *q = bst_find(t, arr[i]);
-        assert(q != NULL);
-        assert(q->key == arr[i]);
-        assert(p == q);
-        bst_erase(t, p);
-        q = bst_find(t, arr[i]);
-        assert(q == NULL);
-    }
+  for (int i = 0; i < n; i++) {
+    node_t *p = bst_find(t, arr[i]);
+    assert(p == NULL);
+  }
+
+  for (int i = 0; i < n; i++) {
+    node_t *p = bst_insert(t, arr[i]);
+    assert(p != NULL);
+    node_t *q = bst_find(t, arr[i]);
+    assert(q != NULL);
+    assert(q->key == arr[i]);
+    assert(p == q);
+    bst_erase(t, p);
+    q = bst_find(t, arr[i]);
+    assert(q == NULL);
+  }
 }
 
 void test_find_erase_fixed() {
-    const key_t arr[] = {10, 5, 8, 34, 67, 23, 156, 24, 2, 12, 25, 36, 990, 26};
-    const size_t n = sizeof(arr) / sizeof(arr[0]);
-    bst *t = new_bst();
-    assert(t != NULL);
+  const key_t arr[] = {10, 5, 8, 34, 67, 23, 156, 24, 2, 12, 25, 36, 990, 26};
+  const size_t n = sizeof(arr) / sizeof(arr[0]);
+  bst *t = new_bst();
+  assert(t != NULL);
 
-    test_find_erase(t, arr, n);
+  test_find_erase(t, arr, n);
 
-    delete_bst(t);
+  delete_bst(t);
 }
 
 void test_find_erase_rand(const size_t n, const unsigned int seed) {
-    srand(seed);
-    bst *t = new_bst();
-    key_t *arr = calloc(n, sizeof(key_t));
-    for (int i = 0; i < n; i++)
-        arr[i] = rand();
+  srand(seed);
+  bst *t = new_bst();
+  key_t *arr = calloc(n, sizeof(key_t));
+  for (int i = 0; i < n; i++)
+    arr[i] = rand();
 
+  test_find_erase(t, arr, n);
 
-    test_find_erase(t, arr, n);
-
-    free(arr);
-    delete_bst(t);
+  free(arr);
+  delete_bst(t);
 }
 
-
 int main(int argc, char *argv[]) {
-    test_init();
-    test_insert_single(123);
-    test_find_single(123, 234);
-    //test_erase_root(128);
-    test_find_erase_fixed();
-    test_minmax_suite();
-    test_find_erase_rand(10000, 17);
-    printf("test success!\n");
+  test_init();
+  test_insert_single(123);
+  test_find_single(123, 234);
+  // test_erase_root(128);
+  test_find_erase_fixed();
+  test_minmax_suite();
+  test_find_erase_rand(10000, 17);
+  printf("test success!\n");
 }
--- src/rbtree.c
+++ src/rbtree.c
@@ -1,21 +1,133 @@
 #include "rbtree.h"
-
+#include <stdio.h>
 #include <stdlib.h>
+
+static void delete (rbtree *t, node_t *n);
+static void left_rotate(rbtree *t, node_t *n);
+static void right_rotate(rbtree *t, node_t *n);
+static void insert_fixup(rbtree *t, node_t *z);
 
 rbtree *new_rbtree(void) {
   rbtree *p = (rbtree *)calloc(1, sizeof(rbtree));
-  // TODO: initialize struct if needed
+  node_t *nil = (node_t *)calloc(1, sizeof(node_t));
+  p->root = nil;
+  p->nil = nil;
+  nil->color = RBTREE_BLACK;
+  p->root->left = nil;
+  p->root->right = nil;
+  p->root->parent = nil;
   return p;
 }
 
+static void delete (rbtree *t, node_t *n) {
+  if (n == t->nil)
+    return;
+  delete (t, n->left);
+  delete (t, n->right);
+  free(n);
+}
+
 void delete_rbtree(rbtree *t) {
-  // TODO: reclaim the tree nodes's memory
+  delete (t, t->root);
+  free(t->nil);
   free(t);
 }
 
+static void left_rotate(rbtree *t, node_t *n) {
+  node_t *temp = n->right;
+  n->right = temp->left;
+  if (temp->left != t->nil)
+    temp->left->parent = n;
+  temp->parent = n->parent;
+  if (n->parent == t->nil)
+    t->root = temp;
+  else if (n == n->parent->left)
+    n->parent->left = temp;
+  else
+    n->parent->right = temp;
+  temp->left = n;
+  n->parent = temp;
+}
+
+static void right_rotate(rbtree *t, node_t *n) {
+  node_t *temp = n->left;
+  n->left = temp->right;
+  if (temp->right != t->nil)
+    temp->right->parent = n;
+  temp->parent = n->parent;
+  if (n->parent == t->nil)
+    t->root = temp;
+  else if (n == n->parent->left)
+    n->parent->left = temp;
+  else
+    n->parent->right = temp;
+  temp->right = n;
+  n->parent = temp;
+}
+
+static void insert_fixup(rbtree *t, node_t *z) {
+  while (z->parent->color == RBTREE_RED) {
+    if (z->parent == z->parent->parent->left) {
+      node_t *y = z->parent->parent->right; // uncle node
+      if (y->color == RBTREE_RED) {
+        z->parent->color = RBTREE_BLACK;
+        y->color = RBTREE_BLACK;
+        z->parent->parent->color = RBTREE_RED;
+        z = z->parent->parent;
+      } else {
+        if (z == z->parent->right) {
+          z = z->parent;
+          left_rotate(t, z);
+        }
+        z->parent->color = RBTREE_BLACK;
+        z->parent->parent->color = RBTREE_RED;
+        right_rotate(t, z->parent->parent);
+      }
+    } else {
+      node_t *y = z->parent->parent->left; // uncle node
+      if (y->color == RBTREE_RED) {
+        z->parent->color = RBTREE_BLACK;
+        y->color = RBTREE_BLACK;
+        z->parent->parent->color = RBTREE_RED;
+        z = z->parent->parent;
+      } else {
+        if (z == z->parent->left) {
+          z = z->parent;
+          right_rotate(t, z);
+        }
+        z->parent->color = RBTREE_BLACK;
+        z->parent->parent->color = RBTREE_RED;
+        left_rotate(t, z->parent->parent);
+      }
+    }
+  }
+  t->root->color = RBTREE_BLACK;
+}
+
 node_t *rbtree_insert(rbtree *t, const key_t key) {
-  // TODO: implement insert
-  return t->root;
+  node_t *x = t->root;
+  node_t *y = t->nil;
+  node_t *z = (node_t *)malloc(sizeof(node_t));
+  z->key = key;
+  while (x != t->nil) {
+    y = x;
+    if (key < x->key)
+      x = x->left;
+    else
+      x = x->right;
+  }
+  z->parent = y;
+  if (y == t->nil)
+    t->root = z;
+  else if (key < y->key)
+    y->left = z;
+  else
+    y->right = z;
+  z->left = t->nil;
+  z->right = t->nil;
+  z->color = RBTREE_RED;
+  insert_fixup(t, z);
+  return z;
 }
 
 node_t *rbtree_find(const rbtree *t, const key_t key) {
